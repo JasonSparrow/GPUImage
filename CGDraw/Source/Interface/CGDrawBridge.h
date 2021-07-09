644f8a89b5dc72996306500804786abd07168a67
Jason <jasonweixin@163.com>
2021-07-09 11:30:12 +0800

    bridge file use c++

---
+++ CGDraw/Source/Interface/CGDrawBridge.h
@@ -0,0 +1,12 @@
+//
+//  CGDrawBridge.h
+//  CGDraw
+//
+//  Created by UFOTO on 2021/7/9.
+//
+
+#ifndef CGDrawBridge_h
+#define CGDrawBridge_h
+
+
+#endif /* CGDrawBridge_h */
---
+++ CGDraw/Source/Interface/CGDrawEngine.cpp
@@ -0,0 +1,24 @@
+//
+//  CGDrawEngine.cpp
+//  CGDraw
+//
+//  Created by UFOTO on 2021/7/9.
+//
+
+#include <stdio.h>
+
+long registerEngine() {
+    return 0;
+}
+
+void setInputData(char *)data {
+    
+}
+
+void register_callback(long handle, CallbackCpp *callback) {
+    
+}
+
+void getRenderTexture(long handle) {
+    
+}
